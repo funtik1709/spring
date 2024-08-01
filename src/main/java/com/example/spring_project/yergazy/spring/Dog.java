458d85b45eeed045a41f2eb4f78635cd86cc6c32
e.nurmaganbetov <e.nurmaganbetov@ddirection.kz>
2024-08-01 15:29:55 +0600

    di completed

--- src/main/java/com/example/spring_project/yergazy/spring/Dog.java
+++ src/main/java/com/example/spring_project/yergazy/spring/Dog.java
@@ -1,6 +1,11 @@
 package com.example.spring_project.yergazy.spring;
 
 public class Dog implements Pet {
+
+    public Dog() {
+        System.out.println("Dog bean is created");
+    }
+
     @Override
     public void say() {
         System.out.println("Bow-Wow");
