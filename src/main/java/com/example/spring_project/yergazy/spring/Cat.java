b99a9cfb93ffe97fd09ebd18bf9b95d308fba17b
e.nurmaganbetov <e.nurmaganbetov@ddirection.kz>
2024-08-05 15:03:40 +0600

    annotations config

--- src/main/java/com/example/spring_project/yergazy/spring/Cat.java
+++ src/main/java/com/example/spring_project/yergazy/spring/Cat.java
@@ -1,5 +1,8 @@
 package com.example.spring_project.yergazy.spring;
 
+import org.springframework.stereotype.Component;
+
+@Component("catBean")
 public class Cat implements Pet{
 
     public Cat() {
